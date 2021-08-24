03a6d549441dc50d77789f01215402593fa395d8
ShradhaNbr <sradha.nambiar@gmail.com>
2021-08-24 09:39:59 +0530

    Added : code to store employee wage into an array

--- Employee Wage.js
+++ Employee Wage.js
@@ -15,13 +15,19 @@ switch(empCheck) {
         return 0;
 }
 }
+function calcDailyWage(empHours) {
+    return empHours*WAGE_PER_HOUR;
+}
 let totalEmpHours = 0;
 let  totalWorkingDays = 0;
+let empDailyWageArray = new Array();
 while(totalEmpHours <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalEmpHours += getWorkingHours(empCheck);
+    empDailyWageArray.push(calcDailyWage(totalEmpHours)); // pushing total employee wage into array
 }
-let empWage = totalEmpHours * WAGE_PER_HOUR;
+let empWage = calcDailyWage(totalEmpHours);
 console.log("Total Days "+ totalWorkingDays + " Employee Hours : " + totalEmpHours + " Employee wage : " + empWage);
+console.log(empDailyWageArray);
 
